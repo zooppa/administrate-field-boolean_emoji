7afc73703e527239160792479815c39c721d28fb
neumayr <neumayr@users.noreply.github.com>
2018-02-27 17:00:51 +0100

    Localize boolean output
    Add default values for 🇬🇧 🇺🇸 🇩🇪 🇪🇸 🇫🇷 🇮🇹 🇳🇱 🇨🇿 🇵🇹 🇷🇺
    🇫🇮 🇯🇵 🇰🇷 that can be easily emojified.

--- lib/administrate/field/boolean_emoji.rb
+++ lib/administrate/field/boolean_emoji.rb
@@ -1,28 +1,13 @@
-require 'administrate/field/base'
 require 'rails'
+require 'administrate/field/base'
 
 module Administrate
   module Field
     class BooleanEmoji < Administrate::Field::Base
       class Engine < ::Rails::Engine; end
 
-      EMOJIS = {
-        default: {
-          positive: '&#9989;',
-          negative: '&#10060;'
-        }
-      }
-
-      def style
-        :default
-      end
-
-      def positive_emoji
-        EMOJIS[style][:positive].html_safe
-      end
-
-      def negative_emoji
-        EMOJIS[style][:negative].html_safe
+      def lboolean
+        data.nil? ? '–' : I18n.t(data.to_s, scope: 'administrate.fields.boolean_emoji')
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'administrate/field/boolean_emoji'
+
+I18n.load_path << Dir[File.join('config', 'locales', '**', '*.{rb,yml}')]
