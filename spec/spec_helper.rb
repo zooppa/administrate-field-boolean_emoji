f3ccc683d738298add15c32dfdbe6291b113689e
Michele Gerarduzzi <hello@michelegera.dev>
2022-01-09 11:34:40 +0100

    Fix RuboCop violations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'administrate/field/boolean_emoji'
 
 I18n.load_path << Dir[File.join('config', 'locales', '**', '*.{rb,yml}')]
